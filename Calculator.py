a92f2b83b951540841c78acce39924de6f26c9c0
Yagnamsetty Bhargava Vishnu <131019470+Yagnamsetty-Bhargava-Vishnu@users.noreply.github.com>
2023-05-29 15:56:48 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,51 @@
+def add(x, y):
+    """Addition"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication"""
+    return x * y
+
+def divide(x, y):
+    """Division"""
+    return x / y
+
+def calculator():
+    
+    while True:
+        print("Select operation:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == '5':
+            print("Exiting the calculator...")
+            break
+
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            if num2 == 0:
+                print("Error: Division by zero")
+            else:
+                print("Result:", divide(num1, num2))
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+# Run the calculator
+calculator()
